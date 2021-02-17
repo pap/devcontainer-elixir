374a47ebae57e2df3863afb456f039d8164b7ab1
Paulo Alves Pereira <paulo.alves.pereira@hey.com>
2021-02-17 08:54:21 +0000

    Build elixir alpine based dev image

---
+++ shell/.welcome.sh
@@ -0,0 +1,7 @@
+echo "Elixir Devcontainer version: $ELIXIR_IMAGE_VERSION\n"
+echo `mix hex.info` | awk '{ print "Elixir:\t" $4 "\nOTP:\t" $6 "\n"}'
+echo "Terminal Docker tools aliases:"
+echo " * alpine: launch an interactive alpine 3.11 container"
+echo " * dive: inspect the layers of a Docker image"
+echo " * ld: run lazydocker in a container"
+eval "$(starship init zsh)"
